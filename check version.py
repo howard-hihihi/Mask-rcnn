f4c24c518ab1e6caf0d2e98c22e0b6d0890db3fe
obsession.oo <howard930513@gmail.com>
2023-11-12 23:31:22 +0800

    mask rcnn

---
+++ check version.py
@@ -0,0 +1,13 @@
+import matplotlib
+import numpy
+import tensorflow as tf
+import tensorflow.keras as keras  # change 
+import sys
+
+print("-----------------------------------------------------------------------------------------")
+print("numpy ---> ", numpy.__version__)
+print("matplotlib ---> ", matplotlib.__version__)
+print("tensorflow version ---> ", tf.__version__)
+print("keras version ---> ", keras.__version__)
+print("Python 版本是：", sys.version)
+print("-----------------------------------------------------------------------------------------")
